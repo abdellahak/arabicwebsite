645ec0383c809d0d062bcf4f6c7ddd8d970fe597
abdellah khouden <khoudenak@gmail.com>
2024-12-19 02:08:08 +0100

    Refactor navbar and listphone functionality; update HTML structure and links

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-var navbar = document.getElementById('navcontainer')
+var navbar = document.getElementById("navcontainer");
 
-window.onscroll = function() {
-
+window.onscroll = function () {
   // pageYOffset or scrollY
   if (window.pageYOffset > 0) {
-    navbar.classList.add('scrolled');
+    navbar.classList.add("scrolled");
   } else {
-    navbar.classList.remove('scrolled');
+    navbar.classList.remove("scrolled");
   }
 };
 
-
 function showlist() {
-    var x = document.getElementById('listphone');
-    if (x.style.display === "block"){
-        x.style.display = "none";
-    }
-    else{
-        x.style.document = "block";
-    }
-};
+  var x = document.getElementById("listphone");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.document = "block";
+  }
+}
 
 function showlink() {
-    var x = document.getElementById("listphone");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+  var x = document.getElementById("listphone");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
+
+const mobileOpt = document.querySelectorAll(".mobileOpt");
+var x = document.getElementById("listphone");
+mobileOpt.forEach(($btn) => {
+  $btn.addEventListener("click", () => {
+    x.style.display = "none";
+  });
+});
+
+window.addEventListener("resize", function () {
+  x.style.display = "none";
+});
